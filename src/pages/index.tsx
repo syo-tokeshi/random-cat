300d0d410e830de43faf6327817d74b27f52771e
syo tokeshi <tokeshi@mfgnt.com>
2022-11-24 18:16:20 +0900

    pagesにアップ

---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+ 
+interface CatCategory {
+  id: number;
+  name: string;
+}
+ 
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+ 
+type SearchCatImageResponse = SearchCatImage[];
+ 
+const fetchCatImage = async () => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = (await res.json()) as SearchCatImageResponse;
+  return result[0];
+};
+ 
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
+ 
+const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+ 
+  const handleClick = async () => {
+    const image = await fetchCatImage();
+    setCatImageUrl(image.url);
+  };
+ 
+  return (
+    <div>
+      <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} width={500} height="auto" />
+      </div>
+    </div>
+  );
+};
+ 
+export default IndexPage;
